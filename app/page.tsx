7369b6da8e89a121ad5b8cd53af95cfdd0353fc3
msmps <7691252+msmps@users.noreply.github.com>
2023-11-19 17:14:56 +0000

    feat: add entry animation

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,16 @@ export default function Home() {
 
   return (
     <section>
-      <h1 className="text-2xl font-bold mb-7">Matt Simpson</h1>
-      <p className="prose prose-neutral dark:prose-invert">
+      <h1 className="text-2xl font-bold mb-7 motion-safe:animate-entry">
+        Matt Simpson
+      </h1>
+      <p className="prose prose-neutral dark:prose-invert motion-safe:animate-entry [--stagger:1]">
         I&apos;m a software engineer, tech enthusiast, and creator from the UK.
         I currently work at <span className="font-semibold">Capital One</span>{" "}
         as a Principal Software Engineer — focusing on customer acquisitions.
       </p>
 
-      <div className="my-16">
+      <div className="my-16 motion-safe:animate-entry [--stagger:2]">
         <h2 className="text-neutral-600 dark:text-neutral-400 mb-4 text-sm">
           <a href="/blog/">Writing</a>
         </h2>
@@ -33,7 +35,7 @@ export default function Home() {
         </div>
       </div>
 
-      <p className="prose prose-neutral dark:prose-invert prose-a:underline-offset-2 prose-a:decoration-neutral-400 hover:prose-a:decoration-neutral-900 prose-a:transition-colors prose-a:duration-200">
+      <p className="prose prose-neutral dark:prose-invert prose-a:underline-offset-2 prose-a:decoration-neutral-400 hover:prose-a:decoration-neutral-900 prose-a:transition-colors prose-a:duration-200 motion-safe:animate-entry [--stagger:3]">
         Let&apos;s hang out{" "}
         <a
           rel="noopener noreferrer"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,21 @@ const config: Config = {
         sans: ["var(--font-geist-sans)"],
         mono: ["var(--font-geist-mono)"],
       },
+      keyframes: {
+        entry: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "none",
+          },
+        },
+      },
+      animation: {
+        entry: "entry .6s both calc(var(--stagger) * 120ms)",
+      },
     },
   },
   future: {
